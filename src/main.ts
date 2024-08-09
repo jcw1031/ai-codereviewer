c58051d2cb44588fd7e25a49d6f3994193f0d253
jcw1031 <jcw001031@gmail.com>
2024-08-09 17:05:22 +0900

    config: openai 버전 업데이트

--- src/main.ts
+++ src/main.ts
@@ -104,9 +104,9 @@ Git diff to review:
 \`\`\`diff
 ${chunk.content}
 ${chunk.changes
-  // @ts-expect-error - ln and ln2 exists where needed
-  .map((c) => `${c.ln ? c.ln : c.ln2} ${c.content}`)
-  .join("\n")}
+    // @ts-expect-error - ln and ln2 exists where needed
+    .map((c) => `${c.ln ? c.ln : c.ln2} ${c.content}`)
+    .join("\n")}
 \`\`\`
 `;
 }
